6bd42e25fdc2a958fbc7d833239a2acec103d678
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-10-16 07:21:16 -0700

    after setup

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux'
+
+// import customerReducer from './customerReducer'
+// import talhaReducer from './talhaReducer'
+// import R_todo from './R_todo'
+// import userReducer from './user/userReducer';
+// import roleReducer from './rolePermissionReducer/roleReducer';
+// import permissionReducer from './rolePermissionReducer/permissionReducer';
+// import moduleListReducer from './moduleListReducer/moduleListReducer';
+
+
+const rootReducer = combineReducers({
+//   customerReducer,
+//   talhaReducer,
+//   R_todo,
+//   userReducer,
+//   roleReducer,
+//   permissionReducer,
+//   moduleListReducer
+})
+
+export default rootReducer
